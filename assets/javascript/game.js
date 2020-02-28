d69bf46bc4a64107ccf122c482025e378091ff6c
ThuyN <thuynguyen.nht20@gmail.com>
2020-02-28 07:24:28 -0800

    added code for adding btn to DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,76 @@
 $(document).ready(function () {
     //Global Variables==================
+    var animalLists = ["birds", "bear", "mammal", "fish", "cat", "turtle", "wolf", "squirrel", "owl"];
 
-    //Functions===================
+    //1. Add the button to the DOM
+    function buttonRendering() {
+
+        // Deleting the movies prior to adding new movies
+        // (this is necessary otherwise we will have repeat buttons)
+        $("#buttons-view").empty();
+
+        for (var i = 0; i < animalLists.length; i++) {
+            //add a variable with new button
+            var animalBtn = $("<button>");
+
+            animalBtn.text(animalLists[i]);
+
+            animalBtn.addClass("animal").attr("data-name", animalLists[i]);
+
+            $("#buttons-view").append(animalBtn);
+
+        }
+    }
+
+    //2. When the search button submit, the input is push to array then push to the DOM
+    $("#add-animal").on("click", function (event) {
+        event.preventDefault();
+
+        var inputAnimal = $("#search-input").val().trim();
+
+        //push to the array
+        animalLists.push(inputAnimal);
+
+        buttonRendering();
+    })
+
+    //3. get the gif from Giphy
+    function displayGif() {
+        var animalDisplay = $(this).attr("data-name");
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalDisplay + "&api_key=4MpjWrAM3wf0qX6G6dbnCaxyWlDJPuHq&limit=10";
+
+        // Creating an AJAX call for the specific movie button being clicked
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+
+            var animalGif = response.data;
+            for (var i = 0; i < animalGif.length; i++) {
+                console.log(animalGif[i].rating)
+                // Creating and storing a div tag
+                var gifArea = $("<div>");
+
+                // Creating a paragraph tag with the result item's rating
+                var p = $("<p>").text("Rating: " + animalGif[i].rating);
+
+
+
+
+            }
+        });
+    }
 
 
 
 
     //GAME RUN=============
+    $(document).on("click", ".animal", displayGif);
 
-    gameBegin();
+    buttonRendering();
+
 
 
 });
